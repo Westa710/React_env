b5fad6bbee7634d1c7ac9ced9954dc4ba3ed5451
ko <pleiades1424@gmail.com>
2024-12-26 22:50:58 +0900

    feat: デフォルトのソースを書き換え

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react"
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+import App from './App';
+
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
