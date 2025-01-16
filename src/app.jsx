4b3e5cfc9baba62f4e55a94d68958c8e9bb391b0
ko <pleiades1424@gmail.com>
2025-01-16 10:58:14 +0900

    change: デバッグ用の初期TODOを削除

--- src/app.jsx
+++ src/app.jsx
@@ -5,33 +5,7 @@ import './App.css'
 import './index.css'
 
 export const App = () => {
-  const [tabs, setTabs] = useState({
-    "買い物": [
-      { name: "食パン", completed: false },
-      { name: "洗剤", completed: false },
-      { name: "目薬", completed: false },
-      { name: "ハンドクリーム", completed: false },
-      { name: "乾電池", completed: false },
-      { name: "ティッシュ", completed: false }
-    ],
-    "課題": [
-      { name: "作文", completed: false },
-      { name: "数学プリント", completed: false },
-      { name: "物理問題集", completed: false }
-    ],
-    "タブ1": [
-      { name: "タスク名1", completed: false },
-      { name: "タスク名2", completed: false },
-      { name: "タスク名3", completed: false },
-      { name: "タスク名4", completed: false }
-    ],
-    "タブ2": [
-      { name: "タスク名4", completed: false },
-      { name: "タスク名5", completed: false },
-      { name: "タスク名7", completed: false },
-      { name: "タスク名8", completed: false }
-    ]
-  });
+  const [tabs, setTabs] = useState({});
 
   const [selectedTab, setSelectedTab] = useState(Object.keys(tabs)[0]);
 
